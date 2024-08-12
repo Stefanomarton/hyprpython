e446b8e8ef9a4ed7151e235277beec1709bdcece
Stefanomarton <sstefanomarton@gmail.com>
2024-08-13 00:34:49 +0200

    feat: file for setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hyprpython",
+    version="0.1",
+    packages=find_packages(),
+    description="Python module for hyprland",
+    author="Stefano Marton",
+    author_email="stefano@marton.dev",
+)
